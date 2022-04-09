5548bd84769d9e80ec01a8ce8dd03052745348bb
Eduardo Silva Borges <edusborges99@hotmail.com>
2022-04-08 22:54:35 -0300

    Adição do assets-images

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:log_tinder/pages/login_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
