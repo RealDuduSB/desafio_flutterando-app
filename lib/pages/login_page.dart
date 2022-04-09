6721e6fe78dfca8c4b0c846784f8c90b4f246948
Eduardo Silva Borges <edusborges99@hotmail.com>
2022-04-08 23:03:54 -0300

    Inicio da adição dos elementos ma pagina de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,12 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Image.asset('assets/images/logo.jpg'),
+        ],
+      ),
+    );
   }
 }
