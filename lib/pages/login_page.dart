1431f3f54d6b28e54c0af902e83347a0bd3c5c2f
Eduardo Silva Borges <edusborges99@hotmail.com>
2022-04-08 23:25:46 -0300

    Adicionados os elementos da pagina de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,11 +11,39 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+}
+
+class _body extends StatelessWidget {
+  const _body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Image.asset('assets/images/logo.jpg'),
-        ],
+      color: Color.fromRGBO(255, 255, 255, 1),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            SizedBox(height: 30),
+            Container(
+              child: Text("Location Changer",
+                  style: GoogleFonts.lato(fontSize: 25, color: Colors.black)),
+            ),
+            SizedBox(height: 15),
+            Text("Plugin app for Tinder",
+                style: GoogleFonts.lato(fontSize: 25, color: Colors.black)),
+            SizedBox(height: 15),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text("Login with Facebook",
+                    style: GoogleFonts.lato(fontSize: 15, color: Colors.black)))
+          ],
+        ),
       ),
     );
   }
